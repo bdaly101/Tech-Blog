0b0921079f0e79d14ccadca148c753ae7f7ef12d
Brendan <daly2b@gmail.com>
2024-02-08 10:21:04 -0500

    Fixed bugs

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  console.log("Error JawsDB not set up")
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
